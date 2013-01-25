a144e564c01c9f3cadbb29abcdbce9d20182d906
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2013-01-25 09:55:45 -0600

    Display of statistics in entries can now be filtered

--- searchdex.rb
+++ searchdex.rb
@@ -5,17 +5,33 @@ root = 'http://www.serebii.net/pokedex-bw/'
 pokedex = Marshal.load File.read('pokedex.bin')
 abilitydex = Marshal.load File.read('abilitydex.bin')
 
-opts = {}
+@opts = {}
 OptionParser.new do |o|
     o.banner = "usage searchdex.rb [options]"
-    o.on('--abilitydex', '-a', 'Search Ability Dex') { opts[:adex] = true }
-    o.on('--id ID', 'Search by ID') { |id| opts[:id] = id }
-    o.on('--name NAME', '-n', 'Search by NAME') { |n| opts[:name] = n }
-    o.on('--ability ABILITY', 'Search by ABILITY') { |a| opts[:ability] = a }
-    o.on('--type TYPE', '-t', 'Search by TYPES') {|t| opts[:type] = t.split(',') }
+    o.on('--abilitydex', '-a', 'Search Ability Dex') { @opts[:adex] = true }
+    o.on('--id ID', 'Search by ID') { |id| @opts[:id] = id }
+    o.on('--name NAME', '-n', 'Search by NAME') { |n| @opts[:name] = n }
+    o.on('--ability ABILITY', 'Search by ABILITY') { |a| @opts[:ability] = a }
+    o.on('--type TYPES', '-t', 'Search by comma-delimited TYPES') {|t| @opts[:type] = t.split(',') }
+    o.on('--stat-display STATS', '-s', 'Limit statistics display to comma-delimited STATS') {|s|
+        @opts[:sd] = []
+        l = %w{h hmin hmax n nmin nmax b bmin bmax}
+        s = s.split(',')
+        abort('Invalid values passed to --stat-display') if !s.all? {|v| l.include?(v) }
+        s.each {|v|
+            if v.length == 1
+                @opts[:sd] << "#{v}min"
+                @opts[:sd] << "#{v}max"
+            else
+                @opts[:sd] << v
+            end
+        }
+    }
     o.on('-h', 'Show this help') { puts '',o,''; exit }
     o.parse!
 end
+
+@opts[:sd] = %w{hmin hmax nmin nmax bmin bmax} if !@opts[:sd]
 
 @immunities = { 'normal' => 'Ghost', 'flying' => 'Ground', 'ground' => 'Electric', 'ghost' => 'Normal, Fighting', 'steel' => 'Poison', 'dark' => 'Psychic' }
 
@@ -35,12 +51,12 @@ def print_dex(entry)
     puts "   Abilities: #{abilities}",''
     puts ' '*12 + "STATISTICS".center(52)
     puts "              HP   - %s" % ["Attack", "Defense", "Sp. Atk.", "Sp. Def.", "Speed"].map { |h| h.ljust(9) }.join('- ')
-    puts "    hindered- %3d  - %s" % [shmin[0], shmin[1..5].map { |s| s.ljust(9) }.join('- ') ]
-    puts "    hindered+ %3d  - %s" % [shmax[0], shmax[1..5].map { |s| s.ljust(9) }.join('- ') ]
-    puts "     neutral- %3d  - %s" % [snmin[0], snmin[1..5].map { |s| s.ljust(9) }.join('- ') ]
-    puts "     neutral+ %3d  - %s" % [snmax[0], snmax[1..5].map { |s| s.ljust(9) }.join('- ') ]
-    puts "  beneficial- %3d  - %s" % [sbmin[0], sbmin[1..5].map { |s| s.ljust(9) }.join('- ') ]
-    puts "  beneficial+ %3d  - %s" % [sbmax[0], sbmax[1..5].map { |s| s.ljust(9) }.join('- ') ]
+    puts "    hindered- %3d  - %s" % [shmin[0], shmin[1..5].map { |s| s.ljust(9) }.join('- ') ] if @opts[:sd].include?('hmin')
+    puts "    hindered+ %3d  - %s" % [shmax[0], shmax[1..5].map { |s| s.ljust(9) }.join('- ') ] if @opts[:sd].include?('hmax')
+    puts "     neutral- %3d  - %s" % [snmin[0], snmin[1..5].map { |s| s.ljust(9) }.join('- ') ] if @opts[:sd].include?('nmin')
+    puts "     neutral+ %3d  - %s" % [snmax[0], snmax[1..5].map { |s| s.ljust(9) }.join('- ') ] if @opts[:sd].include?('nmax')
+    puts "  beneficial- %3d  - %s" % [sbmin[0], sbmin[1..5].map { |s| s.ljust(9) }.join('- ') ] if @opts[:sd].include?('nmin')
+    puts "  beneficial+ %3d  - %s" % [sbmax[0], sbmax[1..5].map { |s| s.ljust(9) }.join('- ') ] if @opts[:sd].include?('bmax')
     puts ''
 end
 
@@ -50,21 +66,21 @@ def print_adex(entry)
     puts " Description: #{e[1]}", "      Effect: #{e[2]}", ''
 end
 
-if opts[:adex]
-    if opts[:name]
-        abilitydex.select {|dex| dex[0] =~ /#{opts[:name]}/i}.each {|e| print_adex(e)}
+if @opts[:adex]
+    if @opts[:name]
+        abilitydex.select {|dex| dex[0] =~ /#{@opts[:name]}/i}.each {|e| print_adex(e)}
     else
         puts "You didn't specify an ability using -n."
     end
     exit
 end
 
-if ! opts.empty?
+if ! @opts.empty?
     filter = pokedex
-    filter = filter.select {|dex| dex['num'] == opts[:id].to_i} if opts[:id]
-    filter = filter.select {|dex| dex['name'] =~ /#{opts[:name]}/i} if opts[:name]
-    filter = filter.select {|dex| dex['abilities'].any? {|a| a =~ /#{opts[:ability]}/i}} if opts[:ability]
-    filter = filter.select {|dex| opts[:type].all? {|t| dex['types'].any? {|d| d =~ /#{t}/i }}} if opts[:type]
+    filter = filter.select {|dex| dex['num'] == @opts[:id].to_i} if @opts[:id]
+    filter = filter.select {|dex| dex['name'] =~ /#{@opts[:name]}/i} if @opts[:name]
+    filter = filter.select {|dex| dex['abilities'].any? {|a| a =~ /#{@opts[:ability]}/i}} if @opts[:ability]
+    filter = filter.select {|dex| @opts[:type].all? {|t| dex['types'].any? {|d| d =~ /#{t}/i }}} if @opts[:type]
     if filter.length > 0
         filter.each {|e| print_dex(e)}
     else
